ea0cde25468cc3e3d540b1a9a8564620a32ecc2b
vitaliihoroshko <vitaliihoroshko@gmail.com>
2021-09-27 01:50:34 +0300

    hw l-2

---
+++ HW Lesson 2/script.js
@@ -0,0 +1,11 @@
+// Написати скріпт, який призведе до краху вкладки в браузері
+
+for (let i = 0; i < 1; i--) {
+   console.log("Hello world!")
+}
+
+// Написати код, який на вході приймає стрічку і змінює всі малі букви на великі, а великі на малі. Тобто ‘Test’ ⇒ ‘tEST’
+
+const str = "Test";
+const newStr = str.split("").map(l => l == l.toLowerCase() ? l.toUpperCase() : l.toLowerCase()).join("");
+console.log(newStr);
