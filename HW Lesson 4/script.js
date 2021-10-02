6c262fbb4617aa785973af00439ce2a250d275b5
vitaliihoroshko <vitaliihoroshko@gmail.com>
2021-10-02 18:19:41 +0300

    moified code

--- HW Lesson 4/script.js
+++ HW Lesson 4/script.js
@@ -2,12 +2,12 @@
 // на виході має повернути строку у camel case (приклад строки у camel case: thisIsExampleOfTheCamelCase),
 // яка б поєднувала в собі всі елементи масиву які є строками.
 
-const arr = [1, "heLLo", { film: "Dune" },"MY", 42, ["qwerty", 777, "filter"],"deAR", 23, "fRiEnD"];
+const arr = [1, "heLLo", " ", { film: "Dune" }, "MY", {}, 42, "", ["qwerty", 777, "filter"], "deAR", 23, "fRiEnD"];
 
 function createCamelCase(arr) {
-   let strArr = arr.filter((e) => typeof e === "string");
+   let strArr = arr.filter((e) => typeof e == "string" && e !== "");
    let firstWord = strArr.splice(0, 1);
-   return firstWord.map(l => l.toLowerCase()) + strArr.map(l => l[0].toUpperCase() + l.slice(1).toLowerCase()).join("");
+   return firstWord.map(l => l.toLowerCase()) + strArr.map(l => l[0].toUpperCase() + l.slice(1).toLowerCase()).join("").trim();
 }
 
 console.log(createCamelCase(arr));
