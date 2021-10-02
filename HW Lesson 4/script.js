64b65ff835e4ad1c91e1e64f392a90f556431290
vitaliihoroshko <vitaliihoroshko@gmail.com>
2021-10-02 23:17:07 +0300

    hw l-4 second and third tasks

--- HW Lesson 4/script.js
+++ HW Lesson 4/script.js
@@ -11,3 +11,27 @@ function createCamelCase(arr) {
 }
 
 console.log(createCamelCase(arr));
+
+// Створити функцію яка отримує параметром масив цілих чисел 1 <= value <= 26, і повертає масиву,
+// у якому кожен елемент є літерою англійського алфавіту, відповідний конкретному елементу масива.
+// Приклад [4, 3, 22, 11] - в результаті маємо отримати [d, c, v, k].
+
+const arr = [10, 1, 22, 1, 19, 3, 18, 9, 16, 20];
+
+function transformNumToChar(arr) {
+   return arr.map(i => String.fromCharCode(i + 96));
+}
+
+console.log(transformNumToChar(arr));
+
+// Створити функцію яка параметром отримує об’єкт (наприклад {a: 22, b: -11.35, c: 41.2, d: ‘hello’}) і повертає новий об’єкт,
+// який містить тільки числа більші рівні 0.
+
+const obj = { a: 22, b: -11.35, c: 41.2, d: "hello" };
+
+function createNumObj(obj) {
+   const entriesArr = Object.entries(obj).filter(([key, value]) => value >= 0);
+   return Object.fromEntries(entriesArr);
+}
+
+console.log(createNumObj(obj));
