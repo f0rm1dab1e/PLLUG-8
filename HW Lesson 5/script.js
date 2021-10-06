fa68f23a85f6ca812cae1834fc800f7ebefa13a1
vitaliihoroshko <vitaliihoroshko@gmail.com>
2021-10-06 23:22:54 +0300

    hw l-5 first part

--- HW Lesson 5/script.js
+++ HW Lesson 5/script.js
@@ -1,1 +1,63 @@
 "use strict";
+
+// Функція яка збільшує розмір шрифту всіх елементів списку animals;
+
+function changeFontSize() {
+   document.querySelector("#animals").style.fontSize = "20px";
+}
+
+changeFontSize();
+
+// Функція яка видаляє всі класи останнього елемента списку animals;
+
+// function removeLastElemClasses() {
+//    document.querySelector("#animals").lastElementChild.removeAttribute("class");
+// }
+
+// removeLastElemClasses();
+
+// Функція яка змінює background-color наступного елемента після елемента з id="cat";
+
+function changeBackgroundColor() {
+   document.querySelector("#cat").nextElementSibling.style.backgroundColor = "green";
+}
+
+changeBackgroundColor();
+
+// Функція яка змінює колір тексту на червоний всім елементам які мають клас dangerous;
+
+function changeTextColor() {
+   document.querySelector(".dangerous").style.color = "red";
+}
+
+changeTextColor();
+
+// Функція яка видаляє всі елементи які мають клас pet, але якщо крім класу pet є ще інші класи, то вони повинні залишитись;
+
+function removeClassPet() {
+   document.querySelectorAll(".pet").forEach(elem => (elem.classList.length === 1) ? elem.remove() : undefined);
+}
+
+removeClassPet();
+
+// Функція яка додає новий елемент в список birds - після цього це має бути список у якому новий елемент є першим;
+
+function addBirdPrepend() {
+   const birds = document.querySelector("#birds");
+   const newBird = document.createElement("li");
+   newBird.textContent = "Dove";
+   birds.prepend(newBird);
+}
+
+addBirdPrepend();
+
+// Функція яка додає новий елемент в список birds - після цього це має бути список у якому новий елемент є останнім;
+
+function addBirdAppend() {
+   const birds = document.querySelector("#birds");
+   const newBird = document.createElement("li");
+   newBird.textContent = "Swan";
+   birds.append(newBird);
+}
+
+addBirdAppend();
